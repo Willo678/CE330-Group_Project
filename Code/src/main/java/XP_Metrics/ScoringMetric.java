9b77715c78ad15efc19d90960b417c3bfb09c959
Oska <of21284@essex.ac.uk>
2024-11-18 22:03:04 +0000

    score tracker of indentation in bracepair

--- Code/src/main/java/XP_Metrics/ScoringMetric.java
+++ Code/src/main/java/XP_Metrics/ScoringMetric.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import static XP_Metrics.getBracePairs.*;
 
 public class ScoringMetric {
+    int score = 0;
 
     String path = "D:\\Year 3\\Visualising Algorithims\\Project - Vis. DS\\24-25_CE301_flatman_oska/Graphs.java";
     int indentationThreshold = 3;
@@ -16,9 +17,14 @@ public class ScoringMetric {
         for (BracePair bracePair : bracePairs) {
             if (bracePair.indentation > indentationThreshold) {
                 System.out.println("BracePair with indentation > " + indentationThreshold + ": " + bracePair);
+                score -= 2;
+
+            }else
+                score +=1;
             }
+        System.out.println ("score for class indentation: " +score);
         }
-    }
+
 
 
     public static void main(String[] args) {
