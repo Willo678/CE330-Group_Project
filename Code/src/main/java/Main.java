4072b7166d163d304bf7e149db2bf018e2db8958
Ferreira <mf22528@essex.ac.uk>
2024-11-19 14:21:49 +0000

    separate tabs added to GUI and targetSelection logic converted to separate JFrames

--- Code/src/main/java/Main.java
+++ Code/src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         programWindow window = new programWindow();
-        //targetSelectionUI UI = new targetSelectionUI(); //I think we should separate our features into diff classes from the window, then use them in the window class
 
     }
 }
