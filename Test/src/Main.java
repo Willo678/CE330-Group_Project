dc4ca81db27e59b70abb5c46ed9c3a0fd6d37b5f
Congchun Ji <cj24178@essex.ac.uk>
2024-11-19 14:45:49 +0000

    Delete useless files.

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,16 +1,9 @@
 
-import XP_Metrics.IndentationChecker;
-import XP_Metrics.classChecker;
-import XP_Metrics.functionChecker;
 import userInterface.*;
-import utils.Tokeniser;
 
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-
         targetSelectionUI UI = new targetSelectionUI();
 
     }
