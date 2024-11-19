8f2f285d1b28b84b2ed03575e44a0a9bca105a4d
Oska Flatman <of21284@essex.ac.uk>
2024-11-19 14:14:28 +0000

    [Driver: Of21284] [Navigator: WP22246]
    function scorer added to the scoring metric class

--- Code/src/main/java/Main.java
+++ Code/src/main/java/Main.java
@@ -4,7 +4,8 @@ import userInterface.*;
 
 public class Main {
     public static void main(String[] args) {
-        //int score = ScoringMetric.analyzeBracePairsIndentation( "D:\\Year 3\\Visualising Algorithims\\Project - Vis. DS\\24-25_CE301_flatman_oska/Graphs.java",3);
+        int score = ScoringMetric.scoreBracePairsIndentation( "M:\\year 3\\ce320 project\\Code\\src\\main\\java\\userInterface/targetSelectionUI.java",3);
+        int score2 = ScoringMetric.scoreBracePairsFunLength("M:\\year 3\\ce320 project\\Code\\src\\main\\java\\userInterface/targetSelectionUI.java",10);
         targetSelectionUI UI = new targetSelectionUI();
 
     }
--- Code/src/main/java/XP_Metrics/ScoringMetric.java
+++ Code/src/main/java/XP_Metrics/ScoringMetric.java
@@ -4,36 +4,56 @@ import java.util.ArrayList;
 
 import static XP_Metrics.getBracePairs.*;
 
-public class ScoringMetric {
+public  class ScoringMetric {
 
 
-    static String path = "D:\\Year 3\\Visualising Algorithims\\Project - Vis. DS\\24-25_CE301_flatman_oska/Graphs.java";
+    //path = "D:\\Year 3\\Visualising Algorithims\\Project - Vis. DS\\24-25_CE301_flatman_oska/Graphs.java";
+    //path = "M:\\year 3\\ce320 project\\Code\\src\\main\\java\\userInterface/targetSelectionUI.java"
 
 
-    public static int analyzeBracePairsIndentation(String path, int indentationThreshold) {
-        int score = 0;
+    public static int scoreBracePairsIndentation(String path, int indentationThreshold) {
+        int indentationScore = 0;
 
         ArrayList<BracePair> bracePairs = getBracePairs(path);
 
         for (BracePair bracePair : bracePairs) {
             if (bracePair.indentation > indentationThreshold) {
                 System.out.println("BracePair with indentation > " + indentationThreshold + ": " + bracePair);
-                score -= 2;
+                indentationScore -= 2;
 
             } else
-                score += 1;
+                indentationScore += 1;
         }
-        System.out.println("score for class indentation: " + score);
-        return score;
+        System.out.println("score for class indentation: " + indentationScore);
+        return indentationScore;
+    }
+
+    public static int scoreBracePairsFunLength(String path, int funLengthThreshold) {
+        int funScore = 0;
+        ArrayList<BracePair> bracePairs = getBracePairs(path);
+        for (BracePair bracePair : bracePairs) {
+            if (bracePair.end - bracePair.start >= funLengthThreshold) {
+                System.out.println("BracePair with function length > " + funLengthThreshold + ": " + bracePair);
+                funScore -= 2;
+
+            } else {
+                funScore += 1;
+            }
+        }
+        System.out.println("score for function length: " + funScore);
+        return funScore;
     }
 }
+
 
 /*
     public static void main(String[] args) {
         ScoringMetric scoringMetric = new ScoringMetric();
-        scoringMetric.analyzeBracePairsIndentation(path,3);
+        scoringMetric.analyzeBracePairsIndentation("M:\\year 3\\ce320 project\\Code\\src\\main\\java\\userInterface/targetSelectionUI.java",10);
     }
-}
 
 
  */
+
+
+
