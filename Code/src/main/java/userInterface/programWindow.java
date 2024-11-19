2b41157ad36b0fc0531c613e468b8dc05224eefe
Ferreira <mf22528@essex.ac.uk>
2024-11-19 14:21:14 +0000

    separate tabs added to GUI and targetSelection logic converted to separate JFrame

--- Code/src/main/java/userInterface/programWindow.java
+++ Code/src/main/java/userInterface/programWindow.java
@@ -7,14 +7,6 @@ public class programWindow extends JFrame {
     JTabbedPane tabbedPane = new JTabbedPane();
 
     public programWindow() {
-        JPanel targetSelectionPanel = new JPanel();
-        targetSelectionPanel.add(new JLabel("Target Selection Panel"));
-
-        JPanel codeDisplayPanel = new JPanel();
-        codeDisplayPanel.add(new JLabel("Code Display Panel"));
-
-        JPanel codeMetricsPanel = new JPanel();
-        codeMetricsPanel.add(new JLabel("Code Metrics Panel"));
 
         tabbedPane.addTab("Target Selection", new targetSelectionUI());
         tabbedPane.addTab("Source Code Display", new sourceCodeDisplayUI());
