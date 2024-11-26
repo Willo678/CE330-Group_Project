7fd268397b0ba869b265be96dced2ec1fcc5e3d3
William Postill <wp22246@essex.ac.uk>
2024-11-26 14:21:43 +0000

    [Driver: of21284] [Navigator: wp22246] Fixed merge conflict

--- Code/src/main/java/XP_Metrics/getTokens.java
+++ Code/src/main/java/XP_Metrics/getTokens.java
@@ -21,9 +21,10 @@ public class getTokens {
             this.name = name;
             this.indentationLevel = indentationLevel;
         }
+
         @Override
         public String toString() {
-            return "[" + start +  "," + type + "," + name +  "," + indentationLevel + "]";
+            return "[" + start + "," + type + "," + name + "," + indentationLevel + "]";
         }
 
         public boolean equals(Object o) {
@@ -36,12 +37,8 @@ public class getTokens {
     //Stores the start and end of a set of curly braces,
     //along with the type (method, class, if, while, etc.) and the name
     public static class BracePair extends Token {
-        public int start;
         public int end;
-        public String type;
-        public String name;
         int nestedness;
-        int indentationLevel;
 
 
 
@@ -70,7 +67,7 @@ public class getTokens {
 
 
     //Returns an arraylist of Bracepairs for a given class
-    public static ArrayList<Token> getBracePairs(String path) {
+    public static ArrayList<Token> getTokens(String path) {
         Scanner scanner;
         try {
             scanner = new Scanner(new File(path));
@@ -94,17 +91,14 @@ public class getTokens {
             lineNum++;
 
             //Removes all non word or brace characters
-            line = line.replaceAll("([\"']).*(\\1)+|[^a-zA-Z0-9{}\"']+", " ");
+            line = line.replaceAll("([\"']).*(\\1)+|[^a-zA-Z0-9{}\"/']+", " ");
 
 
             //Breaks up each line into statements in the case that a line contains multiple statements
             for (String statement : line.split(";")) {
                 if (statement.contains("//")) {
                     tokenArrayList.add(new Token(lineNum, "Comment", statement, indentLevel));
-                    break;
-                }
-                if (statement.contains("import")) {
-                    tokenArrayList.add(new Token(lineNum, "Import", statement, indentLevel));
+                    continue;
                 }
                 if (statement.contains("import")) {
                     tokenArrayList.add(new Token(lineNum, "Import", statement, indentLevel));
@@ -142,7 +136,18 @@ public class getTokens {
         //Sorts arraylist so that pairs that start earlier are first in the array
         tokenArrayList.sort((a, b) -> ((a.start > b.start) ? 1 : -1));
 
+        System.out.println(tokenArrayList);
+
         return tokenArrayList;
+    }
+
+
+    public static List<BracePair> getBracePairs(String path) {
+        return getTokens(path).stream().filter(BracePair.class::isInstance).map(BracePair.class::cast).toList();
+    }
+
+    public static List<Token> getComments(String path) {
+        return getTokens(path).stream().filter(x -> (Objects.equals(((Token) x).type, "Comment"))).toList();
     }
 
     //Returns whether a given string is a Java keyword, an identifier, or a name
