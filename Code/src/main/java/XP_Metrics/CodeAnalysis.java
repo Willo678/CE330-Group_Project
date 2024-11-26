1b439bc27791e5578e98bbe724bed19090033c37
Sonia Joiner <sj22795@essex.ac.uk>
2024-11-26 15:31:26 +0000

    Driver: [sj22795] Navigator: [wp22246]
    Modify CodeAnalysis to add method name analysis

--- Code/src/main/java/XP_Metrics/CodeAnalysis.java
+++ Code/src/main/java/XP_Metrics/CodeAnalysis.java
@@ -8,24 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import XP_Metrics.getTokens.Token;
+
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
 // import a spellcheck library
 
 public class CodeAnalysis {
 
-    public CodeAnalysis(ArrayList<Token> tokenList) {
-        // temporary file
-        // for each line in file
-        // test
+    public static ArrayList<Score> CodeAnalysis(ArrayList<Token> tokenList) {
 
+        ArrayList<Score> scores = new ArrayList<>();
 
-        methodNameAnalysis(tokenList.stream().filter(getTokens.BracePair.class::isInstance).map(getTokens.BracePair.class::cast).toList());
-//        methodCommentAnalysis(tokenList);
+        scores.addAll(methodNameAnalysis(tokenList.stream().filter(getTokens.BracePair.class::isInstance).map(getTokens.BracePair.class::cast).toList()));
+        scores.addAll(methodCommentAnalysis(tokenList));
 //        codeLineTypeAnalysis(tokenList);
-
+        return new ArrayList<>();
     }
 
 
-    public void codeLineTypeAnalysis(List<Token> tokens) {
+    public static ArrayList<Score> codeLineTypeAnalysis(List<Token> tokens) {
+        ArrayList<Score> scores = new ArrayList<>();
         System.out.println("This should check each line and give it a type" +
                 "\ne.g. class, comment, method, whitespace");
         String[] lineType = new String[]{"class", "comment", "method", "code", "whitespace"};
@@ -33,21 +35,49 @@ public class CodeAnalysis {
         // return hashmap?
         // key = line number
         // object: string from lineType
+        return scores;
     }
 
-    public void methodNameAnalysis(List<getTokens.BracePair> bracePairs) {
+    public static ArrayList<Score> methodNameAnalysis(List<getTokens.BracePair> bracePairs) {
+        ArrayList<Score> scores = new ArrayList<>();
         for (getTokens.BracePair bracePair : bracePairs) {
             if (bracePair.type == "METHOD") {
                 String methodName = bracePair.name;
-                System.out.println(methodName);
+                String[] methodWords = methodName.split("(?=[A-Z])");
+                char[] nameChars = methodName.toCharArray();
+                boolean upperCaseFound = false;
+
+                if (methodName.length() <= 4 || methodName.length() > 20){
+                    scores.add(new Score(20, "Method name is not between 2-20 characters"));
+                }
+                if (!(isLowerCase(nameChars[0]))){
+                    scores.add(new Score(20, "First character is not lower case"));
+                }
+
+                for (int i = 0; i < nameChars.length; i++){
+                    if (isUpperCase(nameChars[i])){
+                        upperCaseFound = true;
+                        break;
+                    }
+                }
+
+                if (!(upperCaseFound)){
+                    scores.add(new Score(20, "No uppercase found"));
+                }
+
+                if (!(methodWords.length >= 2)){
+                    scores.add(new Score(20, "Needs at least 2 words"));
+                }
+
             }
 
-
         }
+        return scores;
     }
 
-    public void methodCommentAnalysis(List<Token> tokens) {
-
+    public static ArrayList<Score> methodCommentAnalysis(List<Token> tokens) {
+        ArrayList<Score> scores = new ArrayList<>();
+        return scores;
     }
 
 }
