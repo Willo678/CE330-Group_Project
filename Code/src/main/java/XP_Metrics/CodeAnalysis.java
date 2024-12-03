b77cda1e3200792e5e06ca766e9a59401431068c
sj22795 <sj22795@essex.ac.uk>
2024-12-03 14:15:32 +0000

    method comment analysis first attempt

--- Code/src/main/java/XP_Metrics/CodeAnalysis.java
+++ Code/src/main/java/XP_Metrics/CodeAnalysis.java
@@ -2,12 +2,11 @@ package XP_Metrics;
 // written by sj22795
 // code cleanliness check first draft
 
-import java.io.File;
-import java.io.IOException;
+
+import XP_Metrics.getTokens.Token;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import XP_Metrics.getTokens.Token;
 
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
@@ -26,18 +25,6 @@ public class CodeAnalysis {
     }
 
 
-    public static ArrayList<Score> codeLineTypeAnalysis(List<Token> tokens) {
-        ArrayList<Score> scores = new ArrayList<>();
-        System.out.println("This should check each line and give it a type" +
-                "\ne.g. class, comment, method, whitespace");
-        String[] lineType = new String[]{"class", "comment", "method", "code", "whitespace"};
-
-        // return hashmap?
-        // key = line number
-        // object: string from lineType
-        return scores;
-    }
-
     public static ArrayList<Score> methodNameAnalysis(List<getTokens.BracePair> bracePairs) {
         ArrayList<Score> scores = new ArrayList<>();
         for (getTokens.BracePair bracePair : bracePairs) {
@@ -47,25 +34,25 @@ public class CodeAnalysis {
                 char[] nameChars = methodName.toCharArray();
                 boolean upperCaseFound = false;
 
-                if (methodName.length() <= 4 || methodName.length() > 20){
+                if (methodName.length() <= 4 || methodName.length() > 20) {
                     scores.add(new Score(20, "Method name is not between 2-20 characters"));
                 }
-                if (!(isLowerCase(nameChars[0]))){
+                if (!(isLowerCase(nameChars[0]))) {
                     scores.add(new Score(20, "First character is not lower case"));
                 }
 
-                for (int i = 0; i < nameChars.length; i++){
-                    if (isUpperCase(nameChars[i])){
+                for (int i = 0; i < nameChars.length; i++) {
+                    if (isUpperCase(nameChars[i])) {
                         upperCaseFound = true;
                         break;
                     }
                 }
 
-                if (!(upperCaseFound)){
+                if (!(upperCaseFound)) {
                     scores.add(new Score(20, "No uppercase found"));
                 }
 
-                if (!(methodWords.length >= 2)){
+                if (!(methodWords.length >= 2)) {
                     scores.add(new Score(20, "Needs at least 2 words"));
                 }
 
@@ -77,6 +64,47 @@ public class CodeAnalysis {
 
     public static ArrayList<Score> methodCommentAnalysis(List<Token> tokens) {
         ArrayList<Score> scores = new ArrayList<>();
+        int tokenIndex = 0;
+
+
+        for (; tokenIndex < tokens.size(); tokenIndex++) {
+
+            if (tokens.get(tokenIndex).type == "METHOD") {
+                boolean commentFound = false;
+                boolean endMethod = false;
+
+                while (!commentFound && !endMethod) {
+                    tokenIndex++;
+
+                    if (tokens.get(tokenIndex).type == "COMMENT") {
+                        commentFound = true;
+
+                        if (tokenIndex + 2 <= tokens.size()) {
+                            if (tokens.get(tokenIndex + 1).type == "COMMENT" && tokens.get(tokenIndex + 2).type == "COMMENT") {
+
+                                scores.add(new Score(20, "Method comment block too large"));
+                                System.out.println("method comment block");
+                            }
+                        }
+                    }
+                    if (tokens.get(tokenIndex).type == "METHOD") {
+
+                        endMethod = true;
+                    }
+                }
+                if (!commentFound) {
+
+                    scores.add(new Score(20, "Method needs at least one comment"));
+                    System.out.println("method comment not found");
+                }
+            }
+
+            if (tokenIndex < tokens.size() - 1) {
+                tokenIndex++;
+            }
+
+        }
+
         return scores;
     }
 
