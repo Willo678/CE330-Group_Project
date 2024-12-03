1c8e45286b684093fdb39f5e7867cbab199a2df4
Congchun Ji <cj24178@essex.ac.uk>
2024-12-03 16:05:37 +0000

    Restore code

--- Code/src/main/java/Main.java
+++ Code/src/main/java/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        EvaluateXP tmp = new EvaluateXP("");
+
 
         programWindow window = new programWindow();
 
