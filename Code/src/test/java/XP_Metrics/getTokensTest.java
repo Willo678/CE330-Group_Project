d0bcf1314541121c4637a2d1fb215d9452a0c53f
William Postill <wp22246@essex.ac.uk>
2024-12-03 14:32:26 +0000

    [Driver: wp22246] [Navigator: cj24178] Added test for 'getTokens' class

--- Code/src/test/java/XP_Metrics/getTokensTest.java
+++ Code/src/test/java/XP_Metrics/getTokensTest.java
@@ -1,6 +1,7 @@
 package XP_Metrics;
 
 import java.util.List;
+import java.util.Objects;
 
 import XP_Metrics.getTokens.*;
 
@@ -8,21 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class getTokensTest {
 
+    List<Token> tokens;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        List<Token> tokens = getTokens.getTokens("src/test/java/test_sample_data/sampleCode1.java");
-        List<BracePair> braces = getTokens.getBracePairs("src/test/java/test_sample_data/sampleCode1.java");
-        List<Token> comments = getTokens.getComments("src/test/java/test_sample_data/sampleCode1.java");
+         tokens = getTokens.getTokens("src/test/java/test_sample_data/sampleCode1.java");
+
     }
 
 
 
     @org.junit.jupiter.api.Test
     void getBracePairs() {
+        List<BracePair> braces = getTokens.getBracePairs("src/test/java/test_sample_data/sampleCode1.java");
+        assertIterableEquals(tokens.stream().filter(BracePair.class::isInstance).toList(), braces);
+
+        assertEquals("sampleCode1".toUpperCase(), braces.get(0).name);
+        assertEquals(3, braces.size());
     }
 
     @org.junit.jupiter.api.Test
     void getComments() {
+        List<Token> comments = getTokens.getComments("src/test/java/test_sample_data/sampleCode1.java");
+        assertIterableEquals(tokens.stream().filter(x -> (Objects.equals(((Token) x).type, "Comment"))).toList(), comments);
+
+
+        assertEquals(1, comments.size());
     }
 
 
