77d10eb32399ee7d6aca941ab741692e8260f669
Joshua Faulkner <jf22470@essex.ac.uk>
2024-12-03 14:53:29 +0000

    Added classCheckerTest JUnit test cases

--- Code/src/test/java/XP_Metrics/classCheckerTest.java
+++ Code/src/test/java/XP_Metrics/classCheckerTest.java
@@ -1,12 +1,51 @@
 package XP_Metrics;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import XP_Metrics.getTokens.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class classCheckerTest {
+    List<Token> tokens;
+    String path = "M:\\3rd Year\\CE320\\GROUP_PROJECT\\Code\\src\\main\\java\\XP_Metrics\\indentationChecker.java";
+
+    @BeforeEach
+    void initialiseLists() {
+        tokens = getTokens.getTokens(path);
+    }
 
     @Test
-    void checkImports() {
+    void checkBefore() {
+        int start = 0;
+        int cur = 0;
+        for (Token token : tokens) {
+            if (token.type.equalsIgnoreCase("class")) {
+                start = token.start;
+            }
+            if (cur < start) {
+                assertEquals(token.type, "import");
+            }
+            cur++;
+        }
+    }
+
+    @Test
+    void checkAfter() {
+        int start = 0;
+        int cur = 0;
+        for (getTokens.Token token : tokens) {
+            if (token.type.equalsIgnoreCase("class")) {
+                start = token.start;
+            }
+            if (cur > start) {
+                assertNotEquals(token.type, "import");
+            }
+            cur++;
+        }
     }
 }
