2d3a0eb073d79c692b42a0a3d3e923dc30c0bca0
Congchun Ji <cj24178@essex.ac.uk>
2024-12-06 17:24:00 +0000

    Added bracePair.type != null to check

--- Code/src/main/java/XP_Metrics/CodeAnalysis.java
+++ Code/src/main/java/XP_Metrics/CodeAnalysis.java
@@ -31,41 +31,41 @@ public class CodeAnalysis {
     public static ArrayList<Score> methodNameAnalysis(List<getTokens.BracePair> bracePairs) {
         ArrayList<Score> scores = new ArrayList<>();
         for (getTokens.BracePair bracePair : bracePairs) {
-            if (bracePair.type == "METHOD") {
+            if (bracePair.type != null && bracePair.type.equals("METHOD")) {
                 String methodName = bracePair.name;
-                String[] methodWords = methodName.split("(?=[A-Z])");
-                char[] nameChars = methodName.toCharArray();
-                boolean upperCaseFound = false;
+                if (methodName != null) {
+                    String[] methodWords = methodName.split("(?=[A-Z])");
+                    char[] nameChars = methodName.toCharArray();
+                    boolean upperCaseFound = false;
 
-                if (methodName.length() < 4) {
-                    int score = 4 - methodName.length();
-                    scores.add(new Score(score, "Method name is not between 4-20 characters"));
-                }
-                if(methodName.length() > 20){
-                    int score = methodName.length() - 20;
-                    scores.add(new Score(score, "Method name is not between 4-20 characters"));
-                }
-                if (!(isLowerCase(nameChars[0]))) {
-                    scores.add(new Score(5, "First character is not lower case"));
-                }
+                    if (methodName.length() < 4) {
+                        int score = 4 - methodName.length();
+                        scores.add(new Score(score, "Method name is not between 4-20 characters"));
+                    }
+                    if(methodName.length() > 20){
+                        int score = methodName.length() - 20;
+                        scores.add(new Score(score, "Method name is not between 4-20 characters"));
+                    }
+                    if (!(isLowerCase(nameChars[0]))) {
+                        scores.add(new Score(5, "First character is not lower case"));
+                    }
 
-                for (int i = 0; i < nameChars.length; i++) {
-                    if (isUpperCase(nameChars[i])) {
-                        upperCaseFound = true;
-                        break;
+                    for (char c : nameChars) {
+                        if (isUpperCase(c)) {
+                            upperCaseFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!(upperCaseFound)) {
+                        scores.add(new Score(5, "No uppercase found"));
+                    }
+
+                    if (!(methodWords.length >= 2)) {
+                        scores.add(new Score(5, "Needs at least 2 words"));
                     }
                 }
-
-                if (!(upperCaseFound)) {
-                    scores.add(new Score(5, "No uppercase found"));
-                }
-
-                if (!(methodWords.length >= 2)) {
-                    scores.add(new Score(5, "Needs at least 2 words"));
-                }
-
             }
-
         }
         return scores;
     }
@@ -80,17 +80,22 @@ public class CodeAnalysis {
                 int searchIndex = tokenIndex + 1;
 
                 while (!commentFound && !endMethod && searchIndex < tokens.size()) {
-                    Token currentToken = tokens.get(searchIndex);
-
-                    if (currentToken.type == "COMMENT") {
-                        commentFound = true;
-                        if (searchIndex + 2 < tokens.size() &&
-                                tokens.get(searchIndex + 1).type == "COMMENT" &&
-                                tokens.get(searchIndex + 2).type == "COMMENT") {
-                            scores.add(new Score(3, "Method comment block too large"));
+                    Token searchToken = tokens.get(searchIndex);
+                    if (searchToken.type != null) {
+                        if (searchToken.type.equals("COMMENT")) {
+                            commentFound = true;
+                            if (searchIndex + 2 < tokens.size()) {
+                                Token nextToken = tokens.get(searchIndex + 1);
+                                Token nextNextToken = tokens.get(searchIndex + 2);
+                                if (nextToken.type != null && nextNextToken.type != null &&
+                                        nextToken.type.equals("COMMENT") &&
+                                        nextNextToken.type.equals("COMMENT")) {
+                                    scores.add(new Score(3, "Method comment block too large"));
+                                }
+                            }
+                        } else if (searchToken.type.equals("METHOD")) {
+                            endMethod = true;
                         }
-                    } else if (currentToken.type == "METHOD") {
-                        endMethod = true;
                     }
                     searchIndex++;
                 }
