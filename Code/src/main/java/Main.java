355e1cab42ab36728ce805dec9dd6c91474b220b
Congchun Ji <cj24178@essex.ac.uk>
2024-12-06 16:45:48 +0000

    Running project

--- Code/src/main/java/Main.java
+++ Code/src/main/java/Main.java
@@ -7,9 +7,17 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-
-
-        //programWindow window = new programWindow();
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        SwingUtilities.invokeLater(() -> {
+//            programWindow window = new programWindow();
+//            window.setVisible(true);
+//        });
+//    }//Running
 
         EvaluateXP evaluator = new EvaluateXP("src/main/java/XP_Metrics/CodeAnalysis.java");
         System.out.println("Averaged scores: " + evaluator.normalisedScore());
