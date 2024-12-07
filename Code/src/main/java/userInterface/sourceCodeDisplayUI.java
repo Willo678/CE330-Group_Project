00c36c7e4fd8f66f5a069a2a7167d7a690747cf2
Congchun Ji <cj24178@essex.ac.uk>
2024-12-07 19:16:16 +0000

    Added public getters for testing or external access

--- Code/src/main/java/userInterface/sourceCodeDisplayUI.java
+++ Code/src/main/java/userInterface/sourceCodeDisplayUI.java
@@ -7,12 +7,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class sourceCodeDisplayUI extends JPanel {
-    String fileName;
-    String currentFolder;
+    private String fileName;  // Make fields private to follow good encapsulation practices
+    private String currentFolder;
 
     public void setFileName(String fileName) {
-        // this should be used to select a file from the specified folder
-        // however its not yet possible to check that
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.showOpenDialog(null);
 
@@ -26,25 +24,29 @@ public class sourceCodeDisplayUI extends JPanel {
     public void setCurrentFolder(String currentFolder) {
         this.currentFolder = currentFolder;
     }
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getCurrentFolder() {
+        return currentFolder;
+    }
 
     public sourceCodeDisplayUI() {
         super();
-        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS)); // makes layout easier
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
         JButton chooseSource = new JButton("Choose Source File");
         JTextArea codeArea = new JTextArea();
         chooseSource.addActionListener(e -> setFileName(fileName));
 
-
         this.add(chooseSource);
         this.add(codeArea);
-
     }
 
     public ArrayList<String> getCode(String fileName) throws FileNotFoundException {
-
         try {
-            ArrayList<String> code = new ArrayList<String>();
+            ArrayList<String> code = new ArrayList<>();
             File currentFile = new File(fileName);
             Scanner fileScan = new Scanner(currentFile);
             code.add(fileScan.nextLine());
@@ -54,5 +56,4 @@ public class sourceCodeDisplayUI extends JPanel {
             return null;
         }
     }
-
 }
